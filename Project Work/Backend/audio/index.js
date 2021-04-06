d771457ec7297dca606e114b9deab07bb437eef7
Manan Makwana <68809084+M23102000@users.noreply.github.com>
2021-04-06 22:58:53 +0530

    Update index.js

--- Project Work/Backend/audio/index.js
+++ Project Work/Backend/audio/index.js
@@ -4,6 +4,12 @@ let mainWindow = remote.getCurrentWindow();
 
 let selectFile = document.getElementById("selectFile");
 let audio = null;
+let arr = ['Audio.mp3'];
+
+/*
+arr = ['dskdm', 'ksas', 'scsc'];
+*/
+
 
 selectFile.onclick = async() => {
     let file = await dialog.showOpenDialog(mainWindow, {
@@ -12,20 +18,20 @@ selectFile.onclick = async() => {
             extensions: ["mp3", "wav", "mp4"],
         }, ],
     });
-    console.log(file);
-    console.log(file.filePaths[0]);
+    //console.log(file);
+    //console.log(file.filePaths[0]);
+    arr.push(file.filePaths[0])
+    console.log(arr.length);
+    console.log(arr);
     document.getElementById('manan').href = file.filePaths[0];
+    document.getElementById('playlist').innerHTML = arr.join('<br>');
+    let temp = document.createElement('a')
+    temp.href = file.filePaths[0]
+    temp.innerHTML = 'xyz';
+    console.log(temp);
+    document.getElementsByTagName('body')[0].appendChild(temp);
+    //document.getElementById('playlist').href = arr.join('<br>');
+    //document.getElementById('playlist').href = file.filePaths[0];
     // audio = new Audio(file.filePaths[0]);
     // audio.play();
 }
-
-//const URL = require('url');
-
-/*function handleFiles(event) {
-    var files = event.target.files;
-    console.log(URL.createObjectURL(files[0]))
-    document.getElementById("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-}
-
-document.getElementById("upload").addEventListener("change", handleFiles, false);*/
