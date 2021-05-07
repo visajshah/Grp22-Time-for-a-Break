a40b5a0b639118e8c65fb567ddb197cf89cbe2ea
Aditya Shah <shadyumbrella4@gmail.com>
2021-05-07 14:31:25 +0530

    #19 added delete functionaity in Idea Module

--- Project Work/Source Code/Resources/Idea/idea.js
+++ Project Work/Source Code/Resources/Idea/idea.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const { inherits } = require('util');
 
 
+
 var ideas_index = 0;
 var ideasArray = new Array();
 function checkInputText(value,msg) {
@@ -32,7 +33,11 @@ function saveJSON() {
         }
     })
 }
-
+function removeItemOnce(value) {
+    const index = ideasArray.findIndex(x => x.idea === value);
+    if (index !== undefined) ideasArray.splice(index, 1);
+    saveJSON();
+}
 function preoloadaddIdeasToPage(idea) {
     var table = document.getElementById("ideaList");
     var tr = document.createElement("tr");
@@ -69,8 +74,40 @@ function addIdeasToPage() {
     saveJSON();
 }
 
+function removeIdea(index) {
+    // Get the element.
+    var row = document.getElementById('idea-' + index);
+    var value = row.cells[1].innerText;
+    console.log(value);
+    //var value = document.querySelector('#idea'+index).value;
 
+    // If we were able to find a row matching that id.
+    if (row) {
+        // Access row's parent node (tbody) and remove the row.
+        row.parentNode.removeChild(row);
+        removeItemOnce(value);
+        
+    }
+}
 
+function removeSelectedIdeas(params) {
+    var rowsToRemove = document.querySelectorAll('input[name=select-row]:checked');
+
+    for (var i = 0; i < rowsToRemove.length; i++) {
+        // Delete the row.
+        removeIdea(rowsToRemove[i].value);
+    }
+}
+
+function toggleSelection(checkbox) {
+    // Get our rows.
+    var rowsToSelect = document.querySelectorAll('input[name=select-row]');
+
+    for (var i = 0; i < rowsToSelect.length; i++) {
+        // Check or uncheck boxes if the 'master' checkbox is checked.
+        rowsToSelect[i].checked = checkbox.checked;
+    }
+}
 
 
 
